43aad28a4465ce9b4304fb66e1157e10b6e2f9ef
Steinar Bragi Sigurðarson <steinar@vivaldi.com>
2017-02-04 12:22:19 +0000

    Renamed a variable

--- src/components/twitter/TweetGenerator.js
+++ src/components/twitter/TweetGenerator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-var intros = ['hello ','Good morning, ','The sun is shining again today thanks to me, ', 'What\'s the deal with the fact that, '];
+var greetings = ['hello ','Good morning, ','The sun is shining again today thanks to me, ', 'What\'s the deal with the fact that, '];
 var subjects = ['lying liberal media. ','obamacare is a disaster. ', 'these trade deals are a disaster. ','mexico is taking our jobs. '];
 var exclamations = ['SAD!','FAKE NEWS','DISHONEST','MAKE AMERICA GREAT AGAIN'];
 
@@ -15,14 +15,14 @@ var TweetGenerator = React.createClass({
     return {sentence: ''};
   },
 
-  sayRandomWord: function()  {
-    var newSentece = intros[getRandomInt(0,4)] + subjects[getRandomInt(0,4)] + exclamations[getRandomInt(0,4)];
+  randomSentence: function()  {
+    var newSentece = greetings[getRandomInt(0,4)] + subjects[getRandomInt(0,4)] + exclamations[getRandomInt(0,4)];
     this.setState({ sentence: newSentece});
   },
   render: function () {
     return (
       <div>
-        <button onClick={this.sayRandomWord}>TWEET</button>
+        <button onClick={this.randomSentence}>TWEET</button>
         <p>{this.state.sentence}</p>
       </div>
 
