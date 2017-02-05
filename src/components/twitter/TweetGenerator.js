252d079b0f0752e91bd7c55c7d02037e0d6cb51e
Steinar Bragi Sigurðarson <steinar@vivaldi.com>
2017-02-05 04:58:57 +0000

    minor TweetGenerator tweak

--- src/components/twitter/TweetGenerator.js
+++ src/components/twitter/TweetGenerator.js
@@ -7,7 +7,7 @@ var greetings = [
   'Liberal media is lying. ',
   '@CNN is in a total meltdown with their FAKE NEWS ' ,
   'I HAVE NOTHING TO DO WITH RUSSIA - NO DEALS, NO LOANS, NO NOTHING! ',
-  'FAKE NEWS - A TOTAL POLITICAL WITCH HUNT!'
+  'FAKE NEWS - A TOTAL POLITICAL WITCH HUNT! '
 ];
 var subjects = [
   'obamacare is a disaster. ',
@@ -18,7 +18,7 @@ var subjects = [
   'We must keep "evil" out of our country! ',
   'Having a good relationship with Russia is a good thing, not a bad thing. Only "stupid" people, or fools, would think that it is bad! ',
   '@katyperry is no bargain but I don\'t like John Mayer--he dates and tells--be careful Katy (just watch!). ',
-  'Amazing how the haters & losers keep tweeting the name “F**kface Von Clownstick” like they are so original & like no one else is doing it...',
+  'Amazing how the haters & losers keep tweeting the name “F**kface Von Clownstick” like they are so original & like no one else is doing it... ',
 ];
 var exclamations = [
   'SAD!',
@@ -40,7 +40,7 @@ var TweetGenerator = React.createClass({
   },
 
   randomSentence: function()  {
-    var newSentence = greetings[getRandomInt(0,4)] + subjects[getRandomInt(0,8)] + exclamations[getRandomInt(0,5)];
+    var newSentence = greetings[getRandomInt(0,4)] + subjects[getRandomInt(0,9)] + exclamations[getRandomInt(0,6)];
     this.setState({ sentences: [newSentence].concat(this.state.sentences)});
   },
   render: function () {
