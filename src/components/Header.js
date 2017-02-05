b82eefe6f936296c95a623964ca56eec85828400
Steinar Bragi Sigurðarson <steinar@vivaldi.com>
2017-02-05 06:07:48 +0000

    Added header image, changed title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Clock from './Clock.js';
+import TrumpCover from '../assets/img/trumpcover.jpg';
 
 var Header = React.createClass({
   render: function () {
     return (
-      <div className="App-header">
-        <Clock />
-        <h2>TrumpOS</h2>
+      <div style={{background:'url('+TrumpCover+')center center',backgroundSize:'cover'}} className="App-header">
+        <h1 style={{margin:'70px 0px'}}>Trump's tweet generator</h1>
       </div>
     );
   }
