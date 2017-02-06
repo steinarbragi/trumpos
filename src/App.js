cf374124a70d61b7047807eea5fa6096ecab8ea3
Steinar Bragi Sigurðarson <steinar@vivaldi.com>
2017-02-06 18:17:07 +0000

    Optimized trump wallpaper, added link to poster sales site

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import AppLauncher from './components/os/AppLauncher.js';
 import AppDock from './components/os/AppDock.js';
 import AppGrid from './components/os/AppGrid.js';
 import TwitterAppIcon from './assets/img/apps/twitterAppIcon.png';
+import EtsyAppIcon from './assets/img/apps/etsyAppIcon.png';
 import HomeAppIcon from './assets/img/apps/home.png';
-import Wallpaper from './assets/img/apps/wallpaper.jpg';
+import Wallpaper from './assets/img/trumpWallpaper.jpg';
 
 const appGridStyle = {
   background: 'url(' + Wallpaper + ') no-repeat center center fixed',
@@ -24,19 +25,23 @@ class App extends Component {
 
   }
 
-  handleClick(e,AppView) {
+  setView(e,AppView) {
     this.setState({view: AppView});
+  }
+  handleRedirect(e, url) {
+    window.location = url;
   }
   render() {
     if (this.state.view==='AppGrid') {
       return (
         <div style={appGridStyle} className="App">
           <AppGrid>
-              <AppLauncher onAppClick={e => this.handleClick(e, 'TwitterView')} name="TrumpTweet" image={TwitterAppIcon} />
+              <AppLauncher onAppClick={e => this.setView(e, 'TwitterView')} name="TrumpTweet" image={TwitterAppIcon} />
+              <AppLauncher onAppClick={e => this.handleRedirect(e, 'https://www.etsy.com/listing/291408839/the-donald-trump-print-various-sizes?ref=hp_rv')} name="Buy Poster" image={EtsyAppIcon} />
           </AppGrid>
           <AppDock>
-            <AppLauncher onAppClick={e => this.handleClick(e, 'TwitterView')} name="TrumpTweet" image={TwitterAppIcon} />
-            <AppLauncher onAppClick={e => this.handleClick(e, 'AppGrid')} name="Home" image={HomeAppIcon} />
+            <AppLauncher onAppClick={e => this.setView(e, 'TwitterView')} name="TrumpTweet" image={TwitterAppIcon} />
+            <AppLauncher onAppClick={e => this.setView(e, 'AppGrid')} name="Home" image={HomeAppIcon} />
           </AppDock>
         </div>
       );
@@ -46,8 +51,8 @@ class App extends Component {
         <div className="App">
           <TwitterView />
           <AppDock>
-            <AppLauncher onAppClick={e => this.handleClick(e, 'TwitterView')} name="TrumpTweet" image={TwitterAppIcon} />
-            <AppLauncher onAppClick={e => this.handleClick(e, 'AppGrid')} name="Home" image={HomeAppIcon} />
+            <AppLauncher onAppClick={e => this.setView(e, 'TwitterView')} name="TrumpTweet" image={TwitterAppIcon} />
+            <AppLauncher onAppClick={e => this.setView(e, 'AppGrid')} name="Home" image={HomeAppIcon} />
           </AppDock>
         </div>
       );
